82257a3a15218449afe3a045358e5ad1e8f3c6f9
Carlos Barron <cbarron@vise.com.mx>
2026-08-12 20:55:49 -0600

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,19 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let basePath = "";
+
+// Si se construye en GitHub Actions, la ruta base es el nombre del repositorio
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: basePath,
   images: {
-    qualities: [75, 90],
-    formats: ['image/avif', 'image/webp'],
+    unoptimized: true, // Requerido para exportación estática (GitHub Pages no tiene optimizador dinámico)
   },
   devIndicators: false,
 };
